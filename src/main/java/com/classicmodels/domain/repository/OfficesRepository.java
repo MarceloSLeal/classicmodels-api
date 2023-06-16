5cd6bde2fc0a768ada69e70afeaa40f75cfc1dfd
MarceloSLeal <marcelosleal@outlook.com>
2023-06-16 04:30:10 -0300

    Create the Repositories for the new Entities

---
+++ src/main/java/com/classicmodels/domain/repository/OfficesRepository.java
@@ -0,0 +1,9 @@
+package com.classicmodels.domain.repository;
+
+import com.classicmodels.domain.model.Offices;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfficesRepository extends JpaRepository<Offices, Integer> {
+}
---
+++ src/main/java/com/classicmodels/domain/repository/OrderDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.classicmodels.domain.repository;
+
+import com.classicmodels.domain.model.OrderDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Integer> {
+}
