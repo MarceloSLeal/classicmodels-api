3b1ad48eac4b44a68b9296d834b0c2c3445f0c66
MarceloSLeal <marcelosleal@outlook.com>
2023-06-20 16:55:57 -0300

    Starting to using Bean Validation on fields of Customer entity using the notations, need to create a test after and testing on controller with Post or Put requests

--- src/main/java/com/classicmodels/domain/model/Customer.java
+++ src/main/java/com/classicmodels/domain/model/Customer.java
@@ -1,6 +1,7 @@
 package com.classicmodels.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,18 +17,50 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer customerNumber;
+
+    @NotBlank
+    @Size(max = 50)
     private String customerName;
+
+    @NotBlank
+    @Size(max = 50)
     private String contactLastName;
+
+    @NotBlank
+    @Size(max = 50)
     private String contactFirstName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Pattern(regexp = "[0-9+\\-().\\s]+")
     private String phone;
+
+    @NotBlank
+    @Size(max = 50)
     private String addressLine1;
+
+    @Size(max = 50)
     private String addressLine2;
+
+    @NotBlank
+    @Size(max = 50)
     private String city;
+
+    @Size(max = 50)
     private String state;
+
+    @Size(max = 15)
     private String postalCode;
+
+    @NotBlank
+    @Size(max = 50)
     private String country;
-    private Integer salesRepEmployeeNumber;
+
+    @DecimalMin(value = "0.00", inclusive = true)
+    @DecimalMax(value = "9999999999.99", inclusive = true)
     private Double creditLimit;
 
+    @Pattern(regexp = "[0-9]{10}")
+    private Integer salesRepEmployeeNumber;
 
 }
