07291c4b5f8c50b241cfefc8d332ad0279ae6315
MarceloSLeal <marcelosleal@outlook.com>
2023-07-04 02:20:51 -0300

    Create a Query Methot in repository to find customer by email

--- src/main/java/com/classicmodels/domain/repository/CustomersRepository.java
+++ src/main/java/com/classicmodels/domain/repository/CustomersRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomersRepository extends JpaRepository<Customers, Integer> {
+
+    Customers findByCustomerEmail(String customerEmail);
+
 }
