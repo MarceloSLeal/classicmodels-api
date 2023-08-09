2678181500ceb60715d23a1315417481a6f79759
MarceloSLeal <marcelosleal@outlook.com>
2023-08-09 20:54:48 -0300

    Preparing the Offices Model validations

--- src/main/java/com/classicmodels/domain/model/Offices.java
+++ src/main/java/com/classicmodels/domain/model/Offices.java
@@ -1,11 +1,12 @@
 package com.classicmodels.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -22,13 +23,35 @@ public class Offices {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(max = 50)
     private String city;
+
+    @NotNull
+    @Size(max = 50)
     private String phone;
+
+    @NotNull
+    @Size(max = 50)
     private String addressLine1;
+
+    @Size(max = 50)
     private String addressLine2;
+
+    @Size(max = 50)
     private String state;
+
+    @NotNull
+    @Size(max = 50)
     private String country;
+
+    @NotNull
+    @Size(max = 15)
     private String postalCode;
+
+    @NotNull
+    @Size(max = 10)
     private String territory;
 
 }
