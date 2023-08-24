8c658f488bcbd6a63d916168cc7a15cd85c555c6
MarceloSLeal <marcelosleal@outlook.com>
2023-08-24 02:09:59 -0300

    Payment Model Mapper

---
+++ src/main/java/com/classicmodels/api/mapper/PaymentMapper.java
@@ -0,0 +1,33 @@
+package com.classicmodels.api.mapper;
+
+import com.classicmodels.api.model.PaymentsRepModel;
+import com.classicmodels.api.model.input.PaymentsInput;
+import com.classicmodels.domain.model.Payments;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Component
+public class PaymentMapper {
+
+    private ModelMapper modelMapper;
+
+    public PaymentsRepModel toModel(Payments payments) {
+        return modelMapper.map(payments, PaymentsRepModel.class);
+    }
+
+    public List<PaymentsRepModel> toCollectionModel(List<Payments> payments) {
+        return payments.stream()
+                .map(this::toModel)
+                .collect(Collectors.toList());
+    }
+
+    public Payments toEntity(PaymentsInput paymentsInput) {
+        return modelMapper.map(paymentsInput, Payments.class);
+    }
+
+}
