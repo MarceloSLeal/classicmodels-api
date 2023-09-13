11a93cf32989b33350ab03d3bfa2f6ff1dd9d1ae
MarceloSLeal <marcelosleal@outlook.com>
2023-09-13 02:13:15 -0300

    Make changes to check_number field receive primary key as UUID and not a simple string anymore

--- src/main/resources/db/migration/V008__create-payments-table.sql
+++ src/main/resources/db/migration/V008__create-payments-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `payments` (
   `customer_id` int NOT NULL,
-  `check_number` varchar(50) NOT NULL,
+  `check_number` varchar(36) default (uuid()),
   `payment_date` datetime NOT NULL,
   `amount` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`customer_id`,`check_number`));
+  PRIMARY KEY (`check_number`));
