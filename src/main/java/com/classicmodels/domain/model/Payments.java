1fb303cbd2a3c4c95899f51df3eeb5a355540254
MarceloSLeal <marcelosleal@outlook.com>
2023-10-02 20:16:23 -0300

    I tryed to use MockMvc for test but I can't understand how to use it, maybe another time, or maybe I still using mockito

--- src/main/java/com/classicmodels/domain/model/Payments.java
+++ src/main/java/com/classicmodels/domain/model/Payments.java
@@ -1,29 +1,35 @@
 package com.classicmodels.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.annotations.Formula;
 import org.hibernate.annotations.JdbcTypeCode;
 
 import java.sql.Types;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 @Entity
 @Table(name = "payments")
 public class Payments {
 
+    @EqualsAndHashCode.Include
     @NotNull
     private Long customerId;
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue
     @JdbcTypeCode(Types.VARCHAR)
--- src/test/java/com/classicmodels/api/controller/PaymentsControllerTest.java
+++ src/test/java/com/classicmodels/api/controller/PaymentsControllerTest.java
@@ -1,37 +1,46 @@
 package com.classicmodels.api.controller;
 
+
 import com.classicmodels.api.mapper.PaymentsMapper;
 import com.classicmodels.domain.model.Payments;
 import com.classicmodels.domain.repository.CustomersRepository;
 import com.classicmodels.domain.repository.PaymentsRepository;
 import com.classicmodels.domain.service.PaymentsCatalogService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.Mockito.when;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
-@WebMvcTest(PaymentsController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+//@WebMvcTest(PaymentsController.class)
 public class PaymentsControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private WebApplicationContext wac;
+
+
     @MockBean
     private PaymentsRepository paymentsRepository;
     @MockBean
+    private PaymentsController paymentsController;
+    @MockBean
     private PaymentsMapper paymentsMapper;
+
     @MockBean
     private PaymentsCatalogService paymentsCatalogService;
     @MockBean
@@ -40,42 +49,38 @@ public class PaymentsControllerTest {
     private List<Payments> mockPaymentsList;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         Payments payments1 = new Payments();
         payments1.setPaymentDate(OffsetDateTime.now());
         payments1.setCheckNumber(UUID.fromString("34b97957-543c-11ee-9da2-0242ac120002"));
         payments1.setAmount(1001.0);
+        payments1.setCustomerId(1L);
         Payments payments2 = new Payments();
         payments2.setPaymentDate(OffsetDateTime.now());
         payments2.setCheckNumber(UUID.fromString("6eea3201-543c-11ee-9da2-0242ac120002"));
         payments2.setAmount(1002.0);
+        payments2.setCustomerId(2L);
         Payments payments3 = new Payments();
         payments3.setPaymentDate(OffsetDateTime.now());
         payments3.setCheckNumber(UUID.fromString("9765c73c-543c-11ee-9da2-0242ac120002"));
         payments3.setAmount(1003.0);
+        payments3.setCustomerId(3L);
+        Payments payments4 = new Payments();
+        payments4.setPaymentDate(OffsetDateTime.now());
+        payments4.setCheckNumber(UUID.fromString("defe37cb-565e-11ee-8c61-0242ac120002"));
+        payments4.setAmount(1004.0);
+        payments4.setCustomerId(1L);
 
         mockPaymentsList = new ArrayList<>();
-
         mockPaymentsList.add(payments1);
         mockPaymentsList.add(payments2);
         mockPaymentsList.add(payments3);
-    }
-
-    @Test
-    public void testListar() throws Exception {
-
-        List<Payments> payments = new ArrayList<>();
-        payments = mockPaymentsList;
-
-        when(paymentsRepository.findAll()).thenReturn(payments);
-
-        mockMvc.perform(get("http://localhost:8080/payments"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].amount").value(1001.0))
-                .andExpect(jsonPath("$[1].amount").value(1002.0))
-                .andExpect(jsonPath("$[2].amount").value(1003.0))
-                .andExpect(jsonPath("$[0].checkNumber").value("34b97957-543c-11ee-9da2-0242ac120002"));
+        mockPaymentsList.add(payments4);
 
     }
+
 
 }
+
+
+
