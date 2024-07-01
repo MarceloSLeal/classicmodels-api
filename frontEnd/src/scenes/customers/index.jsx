1531b3135b4795be6e02c2fc71331f21979fd287
MarceloSLeal <marcelosleal@outlook.com>
2024-07-01 02:31:04 -0300

    trying to change data on datagrid and update on database, I need to understand better this library and the component Datagrid from MUI

--- frontEnd/src/scenes/customers/index.jsx
+++ frontEnd/src/scenes/customers/index.jsx
@@ -1,31 +1,170 @@
+import * as React from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import {
+    GridRowModes,
+    DataGrid,
+    GridToolbarContainer,
+    GridActionsCellItem,
+    GridRowEditStopReasons,
+} from "@mui/x-data-grid";
+import Button from '@mui/material/Button';
 import { Box, useTheme } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
 import { Urls } from "../../api/Paths";
 import useFetchData from "../../api/getData";
+
+
+function EditToolbar(props) {
+    const { setRows, setRowModesModel } = props;
+
+    const handleClick = () => {
+        const id = randomId();
+        setRows((oldRows) => [...oldRows, { id, name: '', age: '', isNew: true }]);
+        setRowModesModel((oldModel) => ({
+            ...oldModel,
+            [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+        }));
+    };
+
+    return (
+        <GridToolbarContainer>
+            <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+                Add record
+            </Button>
+        </GridToolbarContainer>
+    );
+}
 
 const Customers = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const url = Urls();
     const { data, loading, error } = useFetchData(url.customers.findAll_Post);
+    ////////
+    const [rowModesModel, setRowModesModel] = React.useState({});
+    const [rows, setRows] = React.useState(data);
+
+    const handleRowEditStop = (params, event) => {
+        if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+            event.defaultMuiPrevented = true;
+        }
+    };
+
+    const handleEditClick = (id) => () => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+    };
+    const handleDeleteClick = (id) => () => {
+        setRows(rows.filter((row) => row.id !== id));
+    };
+    const handleSaveClick = (id) => () => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    }
+    const handleCancelClick = (id) => () => {
+        setRowModesModel({
+            ...rowModesModel,
+            [id]: { mode: GridRowModes.View, ignoreModifications: true },
+        });
+
+        const editedRow = rows.find((row) => row.id === id);
+        if (editedRow.isNew) {
+            setRows(rows.filter((row) => row.id !== id));
+        }
+    };
+    const processRowUpdate = (newRow) => {
+        const updatedRow = { ...newRow, isNew: false };
+        setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+        return updatedRow;
+    };
+
+    const handleRowModesModelChange = (newRowModesModel) => {
+        setRowModesModel(newRowModesModel);
+    };
+    /////////
 
     const columns = [
-        { field: "id", headerName: "ID", flex: 0.5 },
-        { field: "name", headerName: "NAME", flex: 1, cellClassName: "name-column--cell" },
-        { field: "email", headerName: "EMAIL", flex: 1 },
-        { field: "contactLastName", headerName: "CONT.LAST NAME", flex: 1 },
-        { field: "contactFirstName", headerName: "CONT.FIRST NAME", flex: 1 },
-        { field: "phone", headerName: "PHONE", flex: 1 },
-        { field: "addressLine1", headerName: "ADRESS1", flex: 1 },
-        { field: "addressLine2", headerName: "ADRESS2", flex: 1 },
-        { field: "city", headerName: "CITY", flex: 1 },
-        { field: "state", headerName: "STATE", flex: 1 },
-        { field: "postalCode", headerName: "P.CODE", flex: 1 },
-        { field: "country", headerName: "COUNTRY", flex: 1 },
-        { field: "creditLimit", headerName: "CREDIT", flex: 1 },
-        { field: "employeeId", headerName: "EMP.ID", flex: 1 },
+        { field: "id", headerName: "ID", flex: 0.5, editable: true, },
+        {
+            field: "name", headerName: "NAME", flex: 1,
+            cellClassName: "name-column--cell", editable: true
+        },
+        { field: "email", headerName: "EMAIL", flex: 1, editable: true },
+        {
+            field: "contactLastName", headerName: "CONT.LAST NAME", flex: 1,
+            editable: true
+        },
+        {
+            field: "contactFirstName", headerName: "CONT.FIRST NAME", flex: 1,
+            editable: true
+        },
+        { field: "phone", headerName: "PHONE", flex: 1, editable: true },
+        {
+            field: "addressLine1", headerName: "ADRESS1", flex: 1,
+            editable: true
+        },
+        {
+            field: "addressLine2", headerName: "ADRESS2", flex: 1,
+            editable: true
+        },
+        { field: "city", headerName: "CITY", flex: 1, editable: true },
+        { field: "state", headerName: "STATE", flex: 1, editable: true },
+        { field: "postalCode", headerName: "P.CODE", flex: 1, editable: true },
+        { field: "country", headerName: "COUNTRY", flex: 1, editable: true },
+        { field: "creditLimit", headerName: "CREDIT", flex: 1, editable: true },
+        { field: "employeeId", headerName: "EMP.ID", flex: 1, editable: true },
+
+        /////////
+        {
+            field: 'actions',
+            type: 'actions',
+            headerName: 'Actions',
+            width: 100,
+            cellClassName: 'actions',
+            getActions: ({ id }) => {
+                const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+                if (isInEditMode) {
+                    return [
+                        <GridActionsCellItem
+                            icon={<SaveIcon />}
+                            label="Save"
+                            sx={{
+                                color: 'primary.main',
+                            }}
+                            onClick={handleSaveClick(id)}
+                        />,
+                        <GridActionsCellItem
+                            icon={<CancelIcon />}
+                            label="Cancel"
+                            className="textPrimary"
+                            onClick={handleCancelClick(id)}
+                            color="inherit"
+                        />,
+                    ];
+                }
+
+                return [
+                    <GridActionsCellItem
+                        icon={<EditIcon />}
+                        label="Edit"
+                        className="textPrimary"
+                        onClick={handleEditClick(id)}
+                        color="inherit"
+                    />,
+                    <GridActionsCellItem
+                        icon={<DeleteIcon />}
+                        label="Delete"
+                        onClick={handleDeleteClick(id)}
+                        color="inherit"
+                    />,
+                ];
+            },
+        },
+        ////////
     ];
 
     if (loading) {
@@ -80,6 +219,18 @@ const Customers = () => {
                 <DataGrid
                     rows={data}
                     columns={columns}
+                    editMode="row"
+                    rowModesModel={rowModesModel}
+                    onRowModesModelChange={handleRowModesModelChange}
+                    onRowEditStop={handleRowEditStop}
+                    processRowUpdate={processRowUpdate}
+                    slots={{
+                        toolbar: EditToolbar,
+                    }}
+                    slotProps={{
+                        toolbar: { setRows, setRowModesModel },
+                    }}
+                    onProcessRowUpdateError={handleRowModesModelChange}
                 />
             </Box>
         </Box>
