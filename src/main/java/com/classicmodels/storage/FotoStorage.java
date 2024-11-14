54ca76cc7984517499b0eaaab28b10a1486a24ca
MarceloSLeal <marcelosleal@outlook.com>
2024-11-14 01:34:52 -0300

    AWS S3
    Starting the implentation on backend to connect to S3 bucket

--- src/main/java/com/classicmodels/storage/FotoStorage.java
+++ src/main/java/com/classicmodels/storage/FotoStorage.java
@@ -6,20 +6,16 @@ import java.util.UUID;
 
 public interface FotoStorage {
 
-    public final String THUMBNAIL_PREFIX = "thumbnail.";
+    public String salvar(MultipartFile file, String nome);
 
-    public String salvar(MultipartFile[] files);
-
-    public byte[] recuperar(String foto);
-
-    public byte[] recuperarThumbnail(String foto);
-
-    public void excluir(String foto);
-
-    public String getUrl(String foto);
-
-    default String renomearArquivo(String nomeOriginal) {
-        return UUID.randomUUID().toString() + "_" + nomeOriginal;
-    }
+//    public byte[] recuperar(String foto);
+//
+//    public void excluir(String foto);
+//
+//    public String getUrl(String foto);
+//
+//    default String renomearArquivo(String nomeOriginal) {
+//        return UUID.randomUUID().toString() + "_" + nomeOriginal;
+//    }
 
 }
--- src/main/java/com/classicmodels/storage/s3/FotoStorageS3.java
+++ src/main/java/com/classicmodels/storage/s3/FotoStorageS3.java
@@ -7,9 +7,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 //@Profile("prod") inserir perfil?
 @AllArgsConstructor
@@ -20,85 +28,126 @@ public class FotoStorageS3 implements FotoStorage {
     //private static final Logger logger = LoggerFactory.getLogger(FotoStorageS3.class);
 
     //TODO -- criar variáveis de ambiente para essas chaves
-    String accessKey = "<AWS Access Key>";
-    String secretKey = "<AWS Secret Key>";
+    String accessKey = "${ACCESS_KEY}";
+    String secretKey = "${SECRET_KEY}";
     AwsCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
 
-    private static final String BUCKET = "awbrewer";
+    String regionName = "${REGION_NAME}";
+    S3Client s3Client = S3Client
+            .builder()
+            .region(Region.of(regionName))
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .build();
 
-   // private AmazonS3 amazonS3;
+    private static final String BUCKET = "${BUCKET}";
 
     @Override
-    public String salvar(MultipartFile[] files) {
-        String novoNome = null;
-        if (files != null && files.length > 0) {
-            MultipartFile arquivo = files[0];
-            novoNome = renomearArquivo(arquivo.getOriginalFilename());
+    public String salvar(MultipartFile file, String nome){
 
-            try {
-                AccessControlList acl = new AccessControlList();
-                acl.grantPermission(GroupGrantee.AllUsers, Permission.Read);
+       if( file != null && file.getSize() > 0) {
 
-                enviarFoto(novoNome, arquivo, acl);
-                enviarThumbnail(novoNome, arquivo, acl);
+           try {
+                enviarFoto(nome, file);
             } catch (IOException e) {
                 throw new RuntimeException("Erro salvando arquivo no S3", e);
             }
-        }
 
-        return novoNome;
+       }
+       return nome;
     }
 
-    @Override
-    public byte[] recuperar(String foto) {
-        InputStream is = amazonS3.getObject(BUCKET, foto).getObjectContent();
-        try {
-            return IOUtils.toByteArray(is);
-        } catch (IOException e) {
-            //logger.error("Não conseguiu recuerar foto do S3", e);
-        }
-        return null;
-    }
+    private Map<String, String> enviarFoto(String nome, MultipartFile arquivo) throws IOException{
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content Type", arquivo.getContentType());
+        metadata.put("Size", String.valueOf(arquivo.getSize()));
+        metadata.put("Environment", "Dev");
 
-    @Override
-    public byte[] recuperarThumbnail(String foto) {
-        return recuperar(FotoStorage.THUMBNAIL_PREFIX + foto);
-    }
+        PutObjectRequest por = PutObjectRequest.builder()
+                        .bucket(BUCKET)
+                                .key(nome)
+                                        .metadata(metadata)
+                                                .build();
 
-    @Override
-    public void excluir(String foto) {
-        amazonS3.deleteObjects(new DeleteObjectsRequest(BUCKET).withKeys(foto, THUMBNAIL_PREFIX + foto));
-    }
+        InputStream inputStream = arquivo.getInputStream();
+        s3Client.putObject(por, RequestBody.fromInputStream(inputStream, arquivo.getSize()));
 
-    @Override
-    public String getUrl(String foto) {
-        if (!StringUtils.isEmpty(foto)) {
-            return "https://s3-sa-east-1.amazonaws.com/awbrewer/" + foto;
-        }
-
-        return null;
-    }
-
-    private ObjectMetadata enviarFoto(String novoNome, MultipartFile arquivo, AccessControlList acl)
-            throws IOException {
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType(arquivo.getContentType());
-        metadata.setContentLength(arquivo.getSize());
-        amazonS3.putObject(new PutObjectRequest(BUCKET, novoNome, arquivo.getInputStream(), metadata)
-                .withAccessControlList(acl));
         return metadata;
     }
 
-    private void enviarThumbnail(String novoNome, MultipartFile arquivo, AccessControlList acl)	throws IOException {
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        Thumbnails.of(arquivo.getInputStream()).size(40, 68).toOutputStream(os);
-        byte[] array = os.toByteArray();
-        InputStream is = new ByteArrayInputStream(array);
-        ObjectMetadata thumbMetadata = new ObjectMetadata();
-        thumbMetadata.setContentType(arquivo.getContentType());
-        thumbMetadata.setContentLength(array.length);
-        amazonS3.putObject(new PutObjectRequest(BUCKET, THUMBNAIL_PREFIX + novoNome, is, thumbMetadata)
-                .withAccessControlList(acl));
-    }
+   // private AmazonS3 amazonS3;
+
+//    @Override
+//    public String salvar(MultipartFile[] files) {
+//        String novoNome = null;
+//        if (files != null && files.length > 0) {
+//            MultipartFile arquivo = files[0];
+//            novoNome = renomearArquivo(arquivo.getOriginalFilename());
+//
+//            try {
+//
+//                AccessControlList acl = new AccessControlList();
+//                acl.grantPermission(GroupGrantee.AllUsers, Permission.Read);
+//
+//                enviarFoto(novoNome, arquivo, acl);
+//                enviarThumbnail(novoNome, arquivo, acl);
+//            } catch (IOException e) {
+//                throw new RuntimeException("Erro salvando arquivo no S3", e);
+//            }
+//        }
+//
+//        return novoNome;
+//    }
+//
+//    @Override
+//    public byte[] recuperar(String foto) {
+//        InputStream is = amazonS3.getObject(BUCKET, foto).getObjectContent();
+//        try {
+//            return IOUtils.toByteArray(is);
+//        } catch (IOException e) {
+//            //logger.error("Não conseguiu recuerar foto do S3", e);
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public byte[] recuperarThumbnail(String foto) {
+//        return recuperar(FotoStorage.THUMBNAIL_PREFIX + foto);
+//    }
+//
+//    @Override
+//    public void excluir(String foto) {
+//        amazonS3.deleteObjects(new DeleteObjectsRequest(BUCKET).withKeys(foto, THUMBNAIL_PREFIX + foto));
+//    }
+//
+//    @Override
+//    public String getUrl(String foto) {
+//        if (!StringUtils.isEmpty(foto)) {
+//            return "https://s3-sa-east-1.amazonaws.com/awbrewer/" + foto;
+//        }
+//
+//        return null;
+//    }
+//
+//    private ObjectMetadata enviarFoto(String novoNome, MultipartFile arquivo, AccessControlList acl)
+//            throws IOException {
+//        ObjectMetadata metadata = new ObjectMetadata();
+//        metadata.setContentType(arquivo.getContentType());
+//        metadata.setContentLength(arquivo.getSize());
+//        amazonS3.putObject(new PutObjectRequest(BUCKET, novoNome, arquivo.getInputStream(), metadata)
+//                .withAccessControlList(acl));
+//        return metadata;
+//    }
+//
+//    private void enviarThumbnail(String novoNome, MultipartFile arquivo, AccessControlList acl)	throws IOException {
+//        ByteArrayOutputStream os = new ByteArrayOutputStream();
+//        Thumbnails.of(arquivo.getInputStream()).size(40, 68).toOutputStream(os);
+//        byte[] array = os.toByteArray();
+//        InputStream is = new ByteArrayInputStream(array);
+//        ObjectMetadata thumbMetadata = new ObjectMetadata();
+//        thumbMetadata.setContentType(arquivo.getContentType());
+//        thumbMetadata.setContentLength(array.length);
+//        amazonS3.putObject(new PutObjectRequest(BUCKET, THUMBNAIL_PREFIX + novoNome, is, thumbMetadata)
+//                .withAccessControlList(acl));
+//    }
 
 }
