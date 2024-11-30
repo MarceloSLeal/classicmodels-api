59adcd1b14a66fd8f5de872ef65ff19540e5c947
MarceloSLeal <marcelosleal@outlook.com>
2024-11-30 03:40:18 -0300

    Token validation
    Save the main customer page and need to set an env variable to take the token

--- frontEnd/src/scenes/customers/index.jsx
+++ frontEnd/src/scenes/customers/index.jsx
@@ -52,7 +52,6 @@ const Customers = () => {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'HwuYQd-zrRw6WfCOUz4VcRrdjHeCGvyL6QD76aQUpdQ',
         },
       });
 
