40417a19e4d7e61a33161be8714a5fe828e8ae96
MarceloSLeal <marcelosleal@outlook.com>
2024-12-28 00:09:58 -0300

    Token cookies
    Handle the refresh request

--- src/main/java/com/classicmodels/security/SecurityFilter.java
+++ src/main/java/com/classicmodels/security/SecurityFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Objects;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -29,34 +27,43 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
 //        String token = this.recoverToken(request);
-        String token = this.recoverCookieToken(request);
-        String validatedToken = tokenService.validateToken(token);
+        String cookieName = determineCookieName(request);
+        String token = this.recoverCookieToken(request, cookieName);
 
-        System.out.println("validatedToken: " + validatedToken);
+//        if (!Objects.equals(token, "") && !Objects.equals(validatedToken, "")) {
+        if (token != null && !token.isEmpty()) {
+            String validatedToken = tokenService.validateToken(token);
 
-        if (!Objects.equals(token, "") && !Objects.equals(validatedToken, "")) {
-
-            UserDetails user = usersRepository.findByLogin(validatedToken);
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        } else {
-            System.out.println("Token nao validado");
+            if (validatedToken != null && !validatedToken.isEmpty()) {
+                UserDetails user = usersRepository.findByLogin(validatedToken);
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                System.out.println("Token nao validado");
+            }
         }
 
         filterChain.doFilter(request, response);
     }
 
-    private String recoverToken(HttpServletRequest request){
-        var authHeader = request.getHeader("Authorization");
-        if(authHeader == null) return null;
-        return authHeader.replace("Bearer ", "");
+    //    private String recoverToken(HttpServletRequest request){
+//        var authHeader = request.getHeader("Authorization");
+//        if(authHeader == null) return null;
+//        return authHeader.replace("Bearer ", "");
+//    }
+    private String determineCookieName(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        if (path.startsWith("/auth/refresh")) {
+            return "refreshToken";
+        }
+        return "token";
     }
-    private String recoverCookieToken(HttpServletRequest request) {
+
+    private String recoverCookieToken(HttpServletRequest request, String cookieName) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
-                if ("token".equals(cookie.getName())) {  // Substitua "token" pelo nome do seu cookie
+                if (cookieName.equals(cookie.getName())) {
                     return cookie.getValue();
                 }
             }
