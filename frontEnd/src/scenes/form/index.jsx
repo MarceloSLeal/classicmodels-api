c182b84e29a1667e01ab126ecd3b4c6d684d0a63
MarceloSLeal <marcelosleal@outlook.com>
2025-03-25 21:36:47 -0300

    Users page
    Need to create a new endpoint to send user roles, I could set the roles on the frontEnd but if I create a new role on backEnd I would need to change the code so create a new endpoint for a thing so simple is boring but is the right way, maybe I need to change the name of this file too, from Form to Users

--- frontEnd/src/scenes/form/index.jsx
+++ frontEnd/src/scenes/form/index.jsx
@@ -1,146 +1,134 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, FormControl, Select, TextField, InputLabel, MenuItem } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 
 const initialValues = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    contact: "",
-    address1: "",
-    address2: "",
+  login: "",
+  password: "",
+  role: "",
 }
 
-const phoneRegExp =
-    /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
-
 const userSchema = yup.object().shape({
-    firstName: yup.string().required("required"),
-    lastName: yup.string().required("required"),
-    email: yup.string().email("invalid email").required("required"),
-    contact: yup
-        .string()
-        .matches(phoneRegExp, "Phone number is not valid")
-        .required("required"),
-    address1: yup.string().required("required"),
-    address2: yup.string().required("required"),
-})
+  login: yup.string().max(50).required("required"),
+  password: yup.string().max(10).required("required"),
+  passwordConfirm: yup.string().oneOf([yup.ref("password"), null], "Passwords must match"),
+  role: yup.string().required("required"),
+});
 
 const Form = () => {
-    const isNonMobile = useMediaQuery("(min-width:600px)");
+  const isNonMobile = useMediaQuery("(min-width:600px)");
 
-    const handleFormSubmit = (values) => {
-        console.log(values);
-    }
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  }
 
-    return <Box m="20px">
-        <Header title="CREATE USER" subtitle="Create a new User Profile" />
+  return <Box m="20px">
+    <Header title="CREATE USER" subtitle="Create a new User Profile" />
 
-        <Formik
-            onSubmit={handleFormSubmit}
-            initialValues={initialValues}
-            validationSchema={userSchema}
-        >
-            {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
-                <form onSubmit={handleSubmit}>
-                    <Box
-                        display="grid"
-                        gap="30px"
-                        gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-                        sx={{
-                            "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-                        }}
-                    >
-                        <TextField
-                            fullWidth
-                            variant="filled"
-                            type="text"
-                            label="First Name"
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                            value={values.firstName}
-                            name="firstName"
-                            error={!!touched.firstName && !!errors.firstName}
-                            helperText={touched.firstName && errors.firstName}
-                            sx={{ gridColumn: "span 2" }}
-                        />
-                        <TextField
-                            fullWidth
-                            variant="filled"
-                            type="text"
-                            label="Last Name"
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                            value={values.lastName}
-                            name="lastName"
-                            error={!!touched.lastName && !!errors.lastName}
-                            helperText={touched.lastName && errors.lastName}
-                            sx={{ gridColumn: "span 2" }}
-                        />
-                        <TextField
-                            fullWidth
-                            variant="filled"
-                            type="text"
-                            label="Email"
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                            value={values.email}
-                            name="email"
-                            error={!!touched.email && !!errors.email}
-                            helperText={touched.email && errors.email}
-                            sx={{ gridColumn: "span 4" }}
-                        />
-                        <TextField
-                            fullWidth
-                            variant="filled"
-                            type="text"
-                            label="Contact Number"
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                            value={values.contact}
-                            name="contact"
-                            error={!!touched.contact && !!errors.contact}
-                            helperText={touched.contact && errors.contact}
-                            sx={{ gridColumn: "span 4" }}
-                        />
-                        <TextField
-                            fullWidth
-                            variant="filled"
-                            type="text"
-                            label="Address 1"
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                            value={values.address1}
-                            name="address1"
-                            error={!!touched.address1 && !!errors.address1}
-                            helperText={touched.address1 && errors.address1}
-                            sx={{ gridColumn: "span 4" }}
-                        />
-                        <TextField
-                            fullWidth
-                            variant="filled"
-                            type="text"
-                            label="Address 2"
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                            value={values.address2}
-                            name="address2"
-                            error={!!touched.address2 && !!errors.address2}
-                            helperText={touched.address2 && errors.address2}
-                            sx={{ gridColumn: "span 4" }}
-                        />
-                    </Box>
-                    <Box display="flex" justifyContent="end" mt="20px">
-                        <Button type="submit" color="secondary" variant="contained">
-                            Create New user
-                        </Button>
-                    </Box>
-                </form>
-            )}
-        </Formik>
-    </Box>
+    <Formik
+      onSubmit={handleFormSubmit}
+      initialValues={initialValues}
+      validationSchema={userSchema}
+    >
+      {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <Box
+            display="grid"
+            gap="30px"
+            gridTemplateColumns="repeat(5, minmax(0, 1fr))"
+            sx={{
+              "& > div": { gridColumn: isNonMobile ? undefined : "span 5" },
+            }}
+          >
+            <TextField
+              fullWidth
+              variant="filled"
+              type="text"
+              label="Login"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.login}
+              name="login"
+              error={!!touched.login && !!errors.login}
+              helperText={touched.login && errors.login}
+              sx={{ gridColumn: "span 2" }}
+            />
+
+            <Box sx={{ gridColumn: "span 3" }} />
+
+            <TextField
+              fullWidth
+              variant="filled"
+              type="password"
+              label="Password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+              name="password"
+              error={!!touched.password && !!errors.password}
+              helperText={touched.password && errors.password}
+              sx={{ gridColumn: "span 1" }}
+            />
+
+            <TextField
+              fullWidth
+              variant="filled"
+              type="password"
+              label="Confirm Password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.passwordConfirm}
+              name="passwordConfirm"
+              error={!!touched.passwordConfirm && !!errors.passwordConfirm}
+              helperText={touched.passwordConfirm && errors.passwordConfirm}
+              sx={{ gridColumn: "span 1" }}
+            />
+
+            <Box sx={{ gridColumn: "span 3" }} />
+
+            <FormControl
+              variant="filled"
+              sx={{ gridColumn: "span 2" }}
+            >
+              <InputLabel id="user-select-role">Role </InputLabel>
+              <Select
+                labelId="user-select-role"
+                id="user-role"
+                name="userRole"
+                value={values.role}
+                onChange={(event) => setFieldValue('userRole', event.target.value)}
+                onBlur={handleBlur}
+                label="User Role"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem key="USER" value="USER" />
+                <MenuItem key="ADMIN" value="ADMIN" />
+
+                {/* {dataEmployeeIdNameList && dataEmployeeIdNameList.map((employee) => ( */}
+                {/*   <MenuItem key={employee.id} value={employee.id}> */}
+                {/*     {employee.id} */}
+                {/*     {" "} {employee.lastName} {employee.firstName} */}
+                {/*     {" "} - {employee.jobTitle} */}
+                {/*   </MenuItem> */}
+                {/* ))} */}
+              </Select>
+            </FormControl>
+
+
+          </Box>
+          <Box display="flex" justifyContent="end" mt="20px">
+            <Button type="submit" color="secondary" variant="contained">
+              Create New user
+            </Button>
+          </Box>
+        </form>
+      )}
+    </Formik>
+  </Box>
 }
 
 export default Form;
