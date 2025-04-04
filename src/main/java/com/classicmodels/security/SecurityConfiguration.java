4917a4446ad85321c101f411c5d074ff9ce086da
MarceloSLeal <marcelosleal@outlook.com>
2025-04-04 04:20:54 -0300

    Security Conf
    Remove the permition to register without authentication, this is only necessary for add the firs admin user

--- src/main/java/com/classicmodels/security/SecurityConfiguration.java
+++ src/main/java/com/classicmodels/security/SecurityConfiguration.java
@@ -44,7 +44,7 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST).hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
