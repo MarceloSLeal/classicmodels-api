e9f98e90011fc58d1f3aa4c82a46445da3bc3197
MarceloSLeal <marcelosleal@outlook.com>
2025-06-12 05:05:39 -0300

    calendar requests
    Need to create the calendar requests for post new events, delete, etc.. all the CRUD process

--- frontend/src/scenes/calendar/index.jsx
+++ frontend/src/scenes/calendar/index.jsx
@@ -14,6 +14,7 @@ import Header from "../../components/Header";
 import { tokens } from "../../theme";
 import useFetchData from "../../api/getData";
 import { Urls } from "../../api/Paths";
+import PostForms from "../../components/formsRequests/PostForms";
 
 const Calendar = () => {
   const theme = useTheme();
@@ -134,6 +135,17 @@ const Calendar = () => {
       selected.event.remove();
     }
   };
+
+  const handleEventPost = async (values) => {
+    try {
+
+      const response = await PostForms(values, urlData.calendar.findAll_Post);
+      const data = await response.json();
+
+    } catch (error) {
+
+    }
+  }
 
   useEffect(() => {
     if (data) {
